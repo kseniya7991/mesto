fe6dcd2132cf03f2f2369f534e46e9955b3fc9a6
Kseniya Stoychikova <kseniya7991@gmail.com>
2021-01-26 07:25:41 +0300

    feat(popup): add popup block for user profile

--- scripts/index.js
+++ scripts/index.js
@@ -1,70 +1,48 @@
 let overlay = document.querySelector('.popup');
 let popupOpenBtn = document.querySelector('.user__edit-button');
 let popupCloseBtn = document.querySelector('.popup__close');
-let popupSaveBtn = document.querySelector('.popup__save-button');
 
 
-let openPopup = function(evt) {
+let togglePopup = function(evt) {
+    console.log('open');
     evt.preventDefault();
-    overlay.classList.add('popup_opened');
-    //console.log('функция сработала');
-}
-popupOpenBtn.addEventListener('click', openPopup);
-
-
-let closePopup = function(evt) {
-    if (evt.target === evt.currentTarget) overlay.classList.remove('popup_opened');
+    overlay.classList.toggle('popup_opened');
 }
 
-let closePopupBtn = function(evt) {
-  evt.preventDefault();
-  overlay.classList.remove('popup_opened');
+function closePopup(evt) {
+    if (evt.target === evt.currentTarget) {
+        togglePopup(evt)
+    }
 }
 
+popupOpenBtn.addEventListener('click',togglePopup);
+popupCloseBtn.addEventListener('click',togglePopup);
+overlay.addEventListener('click',closePopup);
 
-popupCloseBtn.addEventListener('click', closePopupBtn);
-overlay.addEventListener('click', closePopup);
-popupSaveBtn.addEventListener('click', closePopupBtn);
 
-
-let userInfo = document.querySelector('.user__profile-info');
-
-// Находим форму в DOM
 let formElement = document.querySelector('.popup__container');
-//let nameInput = formElement.querySelector('.popup__name');
-//let jobInput = formElement.querySelector('.popup__about');
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-
 
 function formSubmitHandler(evt) {
     evt.preventDefault(); 
+    let editName = document.querySelector('.popup__name');
+    let editAbout = document.querySelector('.popup__about');
+    document.querySelector('.user__name').textContent = `${editName.value}`;
+    document.querySelector('.user__about').textContent = `${editAbout.value}`;
+    overlay.classList.toggle('popup_opened')
+}
+
+formElement.addEventListener('submit', formSubmitHandler); 
+
+
+formElement.addEventListener('keypress', function(evt) {
+    if (evt.keyCode == 13) {
+    evt.preventDefault();
     let editName = document.querySelector('.popup__name');
     let editAbout = document.querySelector('.popup__about');
     document.querySelector('.user__name').innerHTML = `<h1 class="user__name">${editName.value}
     <button class="user__edit-button"></button>
   </h1>`;
     document.querySelector('.user__about').textContent = `${editAbout.value}`;
-    overlay.classList.remove('popup_opened');
-    popupOpenBtn.addEventListener('click', openPopup);
-}
-formElement.addEventListener('submit', formSubmitHandler); 
-
-//function editUser() {
-   //let editName = document.querySelector('.popup__name');
-  // let editAbout = document.querySelector('.popup__about');
-  // document.querySelector('.user__name').innerHTML = `<h1 class="user__name">${editName.value}
-  // <button class="user__edit-button"></button>
- //</h1>`;
- //  document.querySelector('.user__about').textContent = `${editAbout.value}`;
-//  popupOpenBtn.addEventListener('click', openPopup);
-//};
-
-//popupSaveBtn.addEventListener('click', editUser);
-
-
-//closePopup();
-//closePopupBtn();
-//editUser();
-
-
+    overlay.classList.toggle('popup_opened');
+    }
+});
