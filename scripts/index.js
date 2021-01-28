debdf2e022f43c53b7fadca347844fd63080a862
Kseniya Stoychikova <kseniya7991@gmail.com>
2021-01-28 19:11:28 +0300

    hotfixes: second review's fixes

--- scripts/index.js
+++ scripts/index.js
@@ -2,47 +2,38 @@ let overlay = document.querySelector('.popup');
 let popupOpenBtn = document.querySelector('.user__edit-button');
 let popupCloseBtn = document.querySelector('.popup__close');
 let formElement = document.querySelector('.popup__container');
-let editName = document.querySelector('.popup__input_name');
-let editAbout = document.querySelector('.popup__input_about');
+let editName = document.querySelector('.popup_input_name');
+let editAbout = document.querySelector('.popup_input_about');
 let userName = document.querySelector('.user__name');
 let userAbout =  document.querySelector('.user__about');
 
 
 let openPopup = function(evt) {
     overlay.classList.add('popup_opened');
+    editName.value = userName.textContent;
+    editAbout.value = userAbout.textContent;
+
+}
+
+let removePopupOpened = function() {
+    overlay.classList.remove('popup_opened');
 }
 
 let closePopup = function(evt) {
     if (evt.target === evt.currentTarget) 
-        overlay.classList.remove('popup_opened');
+        removePopupOpened();
 }
-
-let btnClosePopup = function (evt) {
-    overlay.classList.remove('popup_opened');
-   }
 
 
 function formSubmitHandler(evt) {
     evt.preventDefault(); 
     userName.textContent = editName.value;
     userAbout.textContent = editAbout.value;
-    btnClosePopup();
+    removePopupOpened();
 }
 
-
-// Такое было в чек-листе. Это не нужно делать в этой работе?//
-//При открытом попапе нажатие на клавишу “Enter” 
-//или кнопку «Сохранить» изменяет на странице информацию о пользователе.
-formElement.addEventListener('keypress', function(evt) {
-    if (evt.keyCode == 13) {
-    evt.preventDefault();
-    userName.textContent = editName.value;
-    userAbout.textContent = editAbout.value;
-    btnClosePopup();
-    }
-});
-
 popupOpenBtn.addEventListener('click', openPopup);
-popupCloseBtn.addEventListener('click', btnClosePopup);
+
 overlay.addEventListener('click',closePopup);
 formElement.addEventListener('submit', formSubmitHandler); 
+popupCloseBtn.addEventListener('click', removePopupOpened);
