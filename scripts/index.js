9b60760ceb3a54c797da6c7f99cd0a3375a9cdcb
Kseniya Stoychikova <kseniya7991@gmail.com>
2021-02-22 20:12:22 +0300

    fixes: rename function js

--- scripts/index.js
+++ scripts/index.js
@@ -10,7 +10,7 @@ const buttonCloseProfilePopup = document.querySelector('.popup-close');
 const buttonCloseAddPopup = document.querySelector('.popup-close_add');
 
 const profileForm = document.querySelector('.popup__form_profile');
-const AddCardForm = document.querySelector('.popup__form_add');
+const addCardForm = document.querySelector('.popup__form_add');
 
 const editName = document.querySelector('.popup__input_text_name');
 const editAbout = document.querySelector('.popup__input_text_about');
